d51c78c714c0ee28c49e06a873e45f270487859d
Ferdinand Malcher <ferdinand@malcher.media>
2024-04-17 10:18:29 +0200

    TypeScript Playground

--- book-rating/src/main.ts
+++ book-rating/src/main.ts
@@ -4,3 +4,42 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+
+
+  ////////////
+
+export class Customer {
+
+  // Constructor Shorthand
+  constructor(public id: number) {
+
+    console.log(this.id);
+
+    setTimeout(() => {
+      console.log('ID:', this.id);
+    }, 2000);
+  }
+
+  /*id: number;
+
+  constructor(id: number) {
+    this.id = id;
+  }*/
+
+  private fooBar(arg: number): string {
+    console.log(this.id);
+    return '';
+  }
+}
+
+
+const myCustomer = new Customer(3);
+
+
+
+const foo = function (arg: number): number {
+  return arg + 1;
+}
+
+const foo2 = (arg: number): number => arg + 1;
