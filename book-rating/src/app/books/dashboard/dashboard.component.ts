04acbdaf707da710aff4a284ae6a215a9a442fdb
Ferdinand Malcher <ferdinand@malcher.media>
2024-04-18 14:05:20 +0200

    Service nutzen, Liste aktualisieren

--- book-rating/src/app/books/dashboard/dashboard.component.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Book } from '../shared/book';
 import { BookComponent } from '../book/book.component';
+import { BookRatingService } from '../shared/book-rating.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -12,7 +13,9 @@ import { BookComponent } from '../book/book.component';
 export class DashboardComponent {
   books: Book[] = [];
 
-  constructor() {
+  // private rs = inject(BookRatingService);
+
+  constructor(private rs: BookRatingService) {
     this.books = [
       {
         isbn: '123',
@@ -29,13 +32,30 @@ export class DashboardComponent {
         rating: 3,
       }
     ];
+
   }
 
   doRateUp(book: Book) {
-    console.log('UP', book);
+    const ratedBook = this.rs.rateUp(book);
+    this.updateList(ratedBook);
   }
 
   doRateDown(book: Book) {
-    console.log('DOWN', book);
+    const ratedBook = this.rs.rateDown(book);
+    this.updateList(ratedBook);
+  }
+
+  private updateList(ratedBook: Book) {
+    // [1,2,3,4,5].map(e => e * 10) // [10, 20, 30, 40, 50]
+    // [1,2,3,4,5,6].filter(e => e > 3) // [4, 5, 6]
+
+    this.books = this.books.map(b => {
+      if (b.isbn === ratedBook.isbn) {
+        return ratedBook;
+      } else {
+        return b;
+      }
+    });
+
   }
 }
