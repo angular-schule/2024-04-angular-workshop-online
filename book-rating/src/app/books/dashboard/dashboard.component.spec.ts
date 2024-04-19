234b4c7d01c0bc918529291fd3cb50df711ebbed
Ferdinand Malcher <ferdinand@malcher.media>
2024-04-19 10:20:34 +0200

    Test Dashboard

--- book-rating/src/app/books/dashboard/dashboard.component.spec.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.spec.ts
@@ -11,7 +11,7 @@ describe('DashboardComponent', () => {
   beforeEach(async () => {
     const ratingMock = {
       rateUp: (b: Book) => b,
-      rateDown: (b: Book) => b,
+      rateDown: (b: Book) => b
     };
 
     await TestBed.configureTestingModule({
@@ -38,5 +38,29 @@ describe('DashboardComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should call service.rateUp for doRateUp() ', () => {});
+  it('should call service.rateUp for doRateUp()', () => {
+    // Service anfordern: das ist aber eigentlich unser ratingMock
+    const service = TestBed.inject(BookRatingService);
+
+    // Buch
+    const testBook = { isbn: '123' } as Book; // Type Assertion – gefährlich, bitte nur in Ausnahmefällen und im Test verwenden
+
+
+    // Methode überwachen
+    // spyOn(service, 'rateUp').and.returnValue(testBook);
+    // spyOn(service, 'rateUp').and.callFake(b => b);
+    // durchleiten zum "Original": Methode wird nicht weggeworfen,
+    // sondern im Hintergrund trotzdem aufgerufen
+    spyOn(service, 'rateUp').and.callThrough();
+
+
+    // Act: Methode in Komponente aufrufen
+    component.doRateUp(testBook);
+
+
+    // prüfen, ob Servicemethode aufgerufen wurde
+    expect(service.rateUp).toHaveBeenCalled();
+    expect(service.rateUp).toHaveBeenCalledOnceWith(testBook);
+
+  });
 });
