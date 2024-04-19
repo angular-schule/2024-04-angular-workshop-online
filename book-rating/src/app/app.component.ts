18a8f277b863506bbcec1dd1a87e29dd23903940
Ferdinand Malcher <ferdinand@malcher.media>
2024-04-19 11:23:22 +0200

    Einführung Observables

--- book-rating/src/app/app.component.ts
+++ book-rating/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DashboardComponent } from './books/dashboard/dashboard.component';
+import { Observable, Observer, Subscriber } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -12,4 +13,36 @@ import { DashboardComponent } from './books/dashboard/dashboard.component';
 })
 export class AppComponent {
   title = 'Book Rating!';
+
+  constructor() {
+
+    function producer(sub: Subscriber<number>) {
+      const result = Math.random();
+      sub.next(result);
+      sub.next(10);
+      sub.next(20);
+
+      setTimeout(() => { sub.next(100); }, 2000)
+      setTimeout(() => { sub.next(200); }, 4000)
+      setTimeout(() => { sub.complete(); }, 6000)
+    }
+
+    const obs: Observer<number> = {
+      next: (e: number) => console.log(e),
+      error: (err: any) => console.error(err),
+      complete: () => console.log('FERTIG'),
+    };
+
+    // producer(obs);
+
+    const myObs$ = new Observable(producer);
+    // myObs$.subscribe(obs);
+
+
+
+
+
+
+
+  }
 }
