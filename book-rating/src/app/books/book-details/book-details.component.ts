bcf041310a0e5e958a14bc89949d9443f09b94ad
Ferdinand Malcher <ferdinand@malcher.media>
2024-04-24 10:45:01 +0200

    Detailseite mit AsyncPipe

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -2,12 +2,13 @@ import { Component, inject } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { BookStoreService } from '../shared/book-store.service';
 import { Book } from '../shared/book';
-import { map, switchMap } from 'rxjs';
+import { Observable, map, switchMap } from 'rxjs';
+import { AsyncPipe, JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-book-details',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, AsyncPipe],
   templateUrl: './book-details.component.html',
   styleUrl: './book-details.component.scss'
 })
@@ -15,7 +16,12 @@ export class BookDetailsComponent {
   private route = inject(ActivatedRoute);
   private bs = inject(BookStoreService);
 
-  book?: Book;
+  book$: Observable<Book>;
+
+  /*book2$ = this.route.paramMap.pipe(
+    map(params => params.get('isbn')!),
+    switchMap(isbn => this.bs.getSingle(isbn))
+  );*/
 
   constructor() {
     // PULL
@@ -23,12 +29,10 @@ export class BookDetailsComponent {
     // console.log(isbn);
 
     // PUSH
-    this.route.paramMap.pipe(
+    this.book$ = this.route.paramMap.pipe(
       map(params => params.get('isbn')!),
       switchMap(isbn => this.bs.getSingle(isbn))
-    ).subscribe(book => {
-      this.book = book;
-    });
+    );
 
   }
 }
