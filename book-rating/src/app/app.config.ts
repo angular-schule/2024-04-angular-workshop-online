9a5d6003888828b8958d32c183d197a7424666b8
Ferdinand Malcher <ferdinand@malcher.media>
2024-04-24 12:35:35 +0200

    NgRx: Feature generieren, Feature-State festlegen, Feature einbinden

--- book-rating/src/app/app.config.ts
+++ book-rating/src/app/app.config.ts
@@ -3,9 +3,11 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
-import { provideStore } from '@ngrx/store';
+import { provideState, provideStore } from '@ngrx/store';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { provideEffects } from '@ngrx/effects';
+import { bookFeature } from './books/store/book.reducer';
+import { BookEffects } from './books/store/book.effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,6 +15,10 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(),
     provideStore(),
     provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
-    provideEffects()
+    provideEffects(),
+
+    // Feature-State anmelden
+    provideState(bookFeature),
+    provideEffects(BookEffects)
 ]
 };
